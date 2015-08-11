93d69317c6602f6e8ed0e9470623feea907f38d4
gongming <gongmingqm10@foxmail.com>
2015-08-11 17:51:03 +0800

    Ming - Add user update and user delete API

--- src/main/java/net/gongmingqm10/tutorial/api/UsersApi.java
+++ src/main/java/net/gongmingqm10/tutorial/api/UsersApi.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,15 +22,17 @@ public class UsersApi {
     @Autowired
     protected UserService userService;
 
-    @RequestMapping(method = RequestMethod.GET, produces="application/json")
-    public @ResponseBody
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public
+    @ResponseBody
     ResponseEntity index() {
         List<User> users = userService.getAllUsers();
         return new ResponseEntity<List<User>>(users, HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.POST, produces="application/json")
-    public @ResponseBody
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    public
+    @ResponseBody
     ResponseEntity create(HttpServletRequest request) {
         String username = request.getParameter("username");
         String city = request.getParameter("city");
@@ -37,7 +40,7 @@ public class UsersApi {
         String telephone = request.getParameter("telephone");
         String avatar = request.getParameter("avatar");
 
-        if (username == null || "".equals(username)) {
+        if (isEmpty(username)) {
             return new ResponseEntity<APIMessage>(new APIMessage("Username should not be null"),
                     HttpStatus.BAD_REQUEST);
         } else {
@@ -50,6 +53,53 @@ public class UsersApi {
             userService.create(user);
             return new ResponseEntity<APIMessage>(new APIMessage("Create success"), HttpStatus.CREATED);
         }
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, produces = "application/json")
+    public
+    @ResponseBody
+    ResponseEntity update(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        String city = request.getParameter("city");
+        String email = request.getParameter("email");
+        String telephone = request.getParameter("telephone");
+        String avatar = request.getParameter("avatar");
+        int userId = Integer.parseInt(request.getParameter("id"));
+
+        if (isEmpty(username)) {
+            return new ResponseEntity<APIMessage>(new APIMessage("Username should not be null"),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        if (userService.getUserById(userId) == null) {
+            return new ResponseEntity<APIMessage>(new APIMessage("Username not exists"),
+                    HttpStatus.BAD_REQUEST);
+        } else {
+            User user = new User().setId(userId).setUsername(username)
+                    .setCity(city)
+                    .setAvatar(avatar)
+                    .setEmail(email)
+                    .setTelephone(telephone);
+
+            userService.create(user);
+            return new ResponseEntity<APIMessage>(new APIMessage("Create success"), HttpStatus.CREATED);
+        }
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "application/json")
+    public
+    @ResponseBody
+    ResponseEntity delete(@PathVariable("id") int userId) {
+        if (userService.getUserById(userId) == null) {
+            return new ResponseEntity<APIMessage>(new APIMessage("User not exists"), HttpStatus.BAD_REQUEST);
+        } else {
+            userService.delete(userId);
+            return new ResponseEntity(HttpStatus.OK);
+        }
+    }
+
+    private boolean isEmpty(String text) {
+        return text == null || "".equals(text);
     }
 
 }
--- src/main/java/net/gongmingqm10/tutorial/mappers/UserMapper.java
+++ src/main/java/net/gongmingqm10/tutorial/mappers/UserMapper.java
@@ -1,6 +1,7 @@
 package net.gongmingqm10.tutorial.mappers;
 
 import net.gongmingqm10.tutorial.model.User;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
@@ -15,4 +16,10 @@ public interface UserMapper {
                     " VALUES ( #{username}, #{city}, #{avatar}, #{email}, #{telephone})"
     )
     void insert(User user);
+
+    @Delete("DELETE FROM users WHERE id=#{userId}")
+    void delete(int userId);
+
+    @Select("SELECT * FROM users WHERE id=#{userid}")
+    User getUserById(int userId);
 }
